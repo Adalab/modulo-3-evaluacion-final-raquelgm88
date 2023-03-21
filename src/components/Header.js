a3f1628690bd2ca868c046ac8fd278fa7739d8be
raquelgm88 <raquel.rgm7@gmail.com>
2023-03-21 18:42:39 +0100

    move code from main to dev branch

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import logo from '../images/logo.png';
+
+  const Header = () => {
+      return (
+      <header className='header'>
+      <img src={logo} alt="Logo Harry Potter" className='header__logo'/>
+    </header>
+    );
+  };
+  export default Header;
