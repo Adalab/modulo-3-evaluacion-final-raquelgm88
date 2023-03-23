32ed67149fb407992db0f5b7505f523df89df141
raquelgm88 <raquel.rgm7@gmail.com>
2023-03-23 02:27:18 +0100

    avoid refresh

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,9 +1,13 @@
 const Filters = ({handleFilter, search, handleHouse}) => {
 
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+  };
+
     return (
-      <form className='form' >
+      <form className='form' onSubmit={handleSubmit}>
         <label className="form__label" htmlFor="search">Busca por personaje: </label>
-        <input className='form__input' type="text" name="search" autoComplete='off' onInput={handleFilter} value={search}/>
+        <input className='form__input' type="search" name="search" autoComplete='off' onInput={handleFilter} value={search}/>
         <label className="form__label" htmlFor="house">Selecciona la casa: </label>
         <select className='form__select' name="house" id="house" onChange={handleHouse}>
           <option value="gryffindor">Gryffindor</option>
